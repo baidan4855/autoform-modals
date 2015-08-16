8e1635e9709e3cee1efac2beea9a4d146cf5c903
蒋凯 <kai.jiang@digitalpublishing.cn>
2015-08-17 00:14:27 +0800

    version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jackjiang:autoform-modals',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Adds bootstrap modals to update doc\'s fields from a template.',
   // URL to the Git repository containing the source code for this package.
